776d701c0686184a3b8e029e4ad9d855455d59fb
diego <diegourieljd@gmail.com>
2022-07-01 12:40:17 -0500

    DB with spring JPA, project added

---
+++ DBSpringJPA/src/main/java/com/diegojimenez/DBSpringJPA/DbSpringJpaApplication.java
@@ -0,0 +1,13 @@
+package com.diegojimenez.DBSpringJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbSpringJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbSpringJpaApplication.class, args);
+	}
+
+}
